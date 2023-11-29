49d25e80ad03f6dd2123a8eb4177573b314af28c
dreamlabo <toddlabo@gmail.com>
2023-11-29 11:38:41 -0700

    Added a section that lists out the order of clicked boxes using templates.

--- scripts.js
+++ scripts.js
@@ -1,59 +1,79 @@
 const boxes = document.querySelectorAll(".box");
+const clickedItemOrderSection = document.getElementById('clicked-order');
 const ACTIVE_CLASS = 'active';
-const timerValue = 750;
-
+const TIMER_VALUE = 750;
 const CLICK_TYPE = 'click';
 const KEYPRESS_TYPE = 'keypress';
-
 const listeners = [CLICK_TYPE, KEYPRESS_TYPE];
 const clickedBoxes = [];
-
 
 boxes.forEach((box) => {
     listeners.forEach(listenerType => {
         switch(listenerType) {
             case CLICK_TYPE: 
-                box.addEventListener('click', () => {
-                    box.classList.add(ACTIVE_CLASS);
-                    setArray(box.id);
-                    
-        
-                    if(boxes.length === clickedBoxes.length) {
-                        setTimeout(unColorBoxes, 500)
-                    }
+                box.addEventListener(CLICK_TYPE, () => {
+                    handleListeners(box);
                 });
                 break;
 
             case KEYPRESS_TYPE: 
-                box.addEventListener('keypress', (Event) => {
+                box.addEventListener(KEYPRESS_TYPE, (Event) => {
                     if (Event.key !== 'Enter') {
                         return
                     }
-                    box.classList.add(ACTIVE_CLASS);
-                    setArray(box.id);
-        
-                    if(boxes.length === clickedBoxes.length) {
-                        box.getBoundingClientRect();
-                        setTimeout(unColorBoxes, 500)
-                    }
+                    handleListeners(box);
                 });
                 break;
         }
     });
 });
 
+function handleListeners(element) {
+    element.classList.add(ACTIVE_CLASS);
+    setArray(element.id);
+    element.setAttribute('tabindex', '-1');
+    
+    if(boxes.length === clickedBoxes.length) {
+        document.activeElement.blur();
+        setTimeout(unColorBoxes, TIMER_VALUE)
+    }
+}
+
 function setArray(id) {
     if(clickedBoxes.includes(id)) {
         return;
     }
     clickedBoxes.push(id);
+    appendBoxOrderSection(id);
 }
 
-function unColorBoxes() {
+function unColorBoxes()  {
     if(clickedBoxes.length > 0) {
         const boxID = clickedBoxes.pop();
         const box = document.getElementById(boxID);
         box.classList.remove(ACTIVE_CLASS);
-        setTimeout(unColorBoxes, 750);
+        removeChild();
+
+        setTimeout(unColorBoxes, TIMER_VALUE);
     }
+    else {
+        boxes.forEach(box => {
+            box.setAttribute('tabindex', '0');
+        })
+    }
+}
+
+function removeChild() {
+    const lastElement = clickedItemOrderSection.lastElementChild;
+    if (lastElement) {
+      lastElement.remove();
+    }
+}
+
+function appendBoxOrderSection(id) {
+    console.log(id)
+    const textTemplate = document.getElementById("clicked-box-template");
+    const item = textTemplate.content.cloneNode(true);
+    item.querySelector('.clicked-item').innerText = id;
+    clickedItemOrderSection.appendChild(item);
 }
